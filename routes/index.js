0b8ba23ff2ed7e9cb151d8362d89b7f6c441aff3
Jerry Reptak <jreptak@gmail.com>
2013-03-31 00:30:47 -0400

    added derp

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+var request = require('request');
+
 
 /*
  * GET home page.
@@ -5,4 +7,27 @@
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
+};
+
+/**
+ * Wrapper for Wunderground Weather API
+ **/
+exports.weather = function(req, res) {
+  var oath_key = 'b1db4d875d549c24';
+  var base = 'http://api.wunderground.com/api/' + oath_key + '/conditions/q/';
+
+  var state, city;
+  state = req.params.state;
+  city = req.params.city;
+  
+  var city = base + state + '/' + city + '.json';
+
+  request(city, function(error, response, body) {
+    if(error) {
+      console.log("--ERROR-- WEATHER: " + error);
+      res.status(500);
+    } else {
+      res.send(body);
+    }
+  });
 };
